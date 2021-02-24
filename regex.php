075317a4a88662b5b210772217ed04ded6334e05
DiamantShala <ds48301@ubt-uni.net>
2021-02-24 23:58:15 +0100

    php part?!

---
+++ regex.php
@@ -0,0 +1,10 @@
+<?php
+
+$usernameReg='^[a-zA-Z0-9_]+{5,}+$'
+
+ $passwordReg='^[a-zA-Z0-9_.!?]+{8,}+$'
+
+ $emailReg='^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$'
+
+ $numberReg='^[0-9]{9,10}$'
+ ?>
